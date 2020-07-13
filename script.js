887ade1b52b81d1ae751913d238e6a4697c1826f
MatveySababa <green.reaper.voice@gmail.com>
2020-07-13 18:03:01 +0300

    Добавили монетки

--- script.js
+++ script.js
@@ -116,8 +116,18 @@ function takemoney() {
   bill.onmouseup = function() {
     window.onmousemove = null;
     if ( inAtm(bill) ) {
-      balance.value = +balance.value + +bill.dataset.cost;  // приводим значения к числам и складываем
-      bill.remove();    // удаляет HTML-элемент со страницы
+      let cashContainer = document.querySelector(".cash-container");
+      bill.style.position = "";
+      bill.style.transform = "rotate(90deg) translateX(25%)";
+      cashContainer.append(bill); // присоединяет к элементу
+      bill.style.transition = "transform 1.5s";
+      setTimeout(() => {                          
+        bill.style.transform = "rotate(90deg) translateX(-75%)";
+        bill.ontransitionend = () => {
+          balance.value = +balance.value + +bill.dataset.cost;  // приводим значения к числам и складываем
+          bill.remove();    // удаляет HTML-элемент со страницы
+        }
+      },10);
     } 
   }
 }  
@@ -151,9 +161,75 @@ function inAtm(bill) {
 }
 
 
+// Получение сдачи, создание элементов с использованием ECMAScript 
 
+let changeButton = document.querySelector(".change-button");
+changeButton.onclick = takeChange;
 
+function takeChange() {        // рекурсивная функция - вызывает саму себя
+  if (+balance.value >= 10) {
+    createCoin ("10");
+    balance.value -= 10;
+    return setTimeout(takeChange, 300);
+  } else if (+balance.value >= 5) {
+    createCoin ("5");
+    balance.value -= 5;
+    return setTimeout(takeChange, 300);
+  } else if (+balance.value >= 2) {
+    createCoin ("2");
+    balance.value -= 2;
+    return setTimeout(takeChange, 300);
+  } else if (+balance.value >= 1) {
+    createCoin ("1");
+    balance.value -= 1;
+    return setTimeout(takeChange, 300);
+  }
+}
 
+function createCoin(cost) {
+  let coinSrc = "";
+  switch (cost) {
+    case "10":
+      coinSrc = "img/10rub.png";
+      break;
+    case "5":
+      coinSrc = "img/5rub.png";
+      break;
+    case "2":
+      coinSrc = "img/2rub.png";
+      break;
+    case "1":
+      coinSrc = "img/1rub.png";
+      break;
+    default:
+    console.log("Плохо")
+  }
+  let changeBox = document.querySelector(".change-box");
+  let changeBoxWidth = changeBox.getBoundingClientRect().width;
+  let changeBoxHeight = changeBox.getBoundingClientRect().height;
+  let coin = document.createElement("img");
+  coin.setAttribute("src", coinSrc)
+  coin.style.width = "50px";
+  coin.style.cursor = "pointer";
+  coin.style.position = "absolute";
+  coin.style.top = Math.floor(Math.random() * (changeBoxHeight - 50)) + "px";
+  coin.style.left = Math.floor(Math.random() * (changeBoxWidth - 50)) + "px";
+  changeBox.append(coin);
+  coin.style.transition = "transform .5s, opacity .5s";
+  coin.style.transform = "translateY(-20%)";
+  setTimeout(() => {
+    coin.style.transform = "translateY(0%)";
+    coin.style.opacity = 1;
+  },10);
+  
+  coin.onclick = () => {
+    coin.style.transform = "translateY(-20%)";
+    coin.style.opacity = 0;
+    coin.ontransitionend = () => {
+      coin.remove();
+    }
+  }
+}
 
 
 
